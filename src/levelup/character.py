ff0927ade6c23c6bdd58aa508976fddd9e917e81
makebootstrap <knowledge464@gmail.com>
2024-02-15 16:38:44 -0500

    updtes to files

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,19 @@
 class Character:
-    name = ""
+        name = ""
+    current_position :Position = Position(-100,-100)
+    map :Map = Map()
 
-    def __init__(self, character_name):
-        self.name = character_name
+    # Since python doesn't do method overloading, this is how we support a constructor with optional parameters
+    def __init__(self, character_name=DEFAULT_CHARACTER_NAME):
+        if character_name.strip() == '':
+            self.name = DEFAULT_CHARACTER_NAME
+        else:
+            self.name = character_name
+
+    def move(self, direction :Direction) -> None:
+        self.current_position = self.map.calculate_new_position(
+            self.current_position, direction)
+    
+    def enter_map(self, map :Map):
+        self.map = map
+        self.current_position = self.map.starting_position
