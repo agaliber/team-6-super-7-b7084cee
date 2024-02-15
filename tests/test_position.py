9eb26884c8e8a0d608b60f9d108794b226e30c78
makebootstrap <knowledge464@gmail.com>
2024-02-15 16:10:40 -0500

    updated test_position.py file

---
+++ tests/test_position.py
@@ -0,0 +1,7 @@
+class TestPositionInitWithXY(TestCase):
+    def test_init(self):
+        ARBITRARY_X = 3
+        ARBITRARY_Y = 4
+        testobj = Position(3,4)
+        self.assertEqual(ARBITRARY_X, testobj.x)
+        self.assertEqual(ARBITRARY_Y, testobj.y)
