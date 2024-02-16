6b7ababa3c645f532e856e5bc6a95f91390d3715
makebootstrap <knowledge464@gmail.com>
2024-02-16 13:05:47 -0500

    revert3

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,39 +1,25 @@
-from unittest import TestCase
-from levelup.character import Character, DEFAULT_CHARACTER_NAME
-from tests.map_double import MapDouble
+from levelup.position import Position
 from levelup.direction import Direction
-from unittest.mock import patch
+from levelup.map import Map
 
-class TestCharacter(TestCase):
+DEFAULT_CHARACTER_NAME = "Character"
+class Character:
+    # In python, we don't use getters. So no getPosition or getName for this class
+    name = ""
+    current_position :Position = Position(-100,-100)
+    map :Map = Map()
 
-    def setUp(self):
-        self.character = Character("MyName")
+    # Since python doesn't do method overloading, this is how we support a constructor with optional parameters
+    def __init__(self, character_name=DEFAULT_CHARACTER_NAME):
+        if character_name.strip() == '':
+            self.name = DEFAULT_CHARACTER_NAME
+        else:
+            self.name = character_name
 
-    @patch.object(Character, "enter_map")
-    def test_enter_map_sets_map_and_updates_position(self, mock_enter_map):
-        stubbed_map = MapDouble()
-        mock_enter_map.return_value = None
-        self.character.enter_map(stubbed_map)
-
-        mock_enter_map.assert_called_once_with(stubbed_map)
-        self.assertEqual(self.character.map, stubbed_map)
-        self.assertEqual(self.character.current_position, stubbed_map.starting_position)
-
-    @patch.object(Character, "move")
-    def test_move_updates_position(self, mock_move):
-        mock_move.return_value = None
-        self.character.map = MapDouble()
-        self.character.move(Direction.EAST)
-
-        mock_move.assert_called_once_with(Direction.EAST)
-        self.assertEqual(self.character.current_position.x, self.character.map.STUBBED_X)
-        self.assertEqual(self.character.current_position.y, self.character.map.STUBBED_Y)
-
-    def test_init_with_name(self):
-        expected_name = "MyName"
-        character = Character(expected_name)
-        self.assertEqual(character.name, expected_name)
-
-    def test_init_with_empty_name(self):
-        character = Character("")
-        self.assertEqual(character.name, DEFAULT_CHARACTER_NAME)
+    def move(self, direction :Direction) -> None:
+        self.current_position = self.map.calculate_new_position(
+            self.current_position, direction)
+    
+    def enter_map(self, map :Map):
+        self.map = map
+        self.current_position = self.map.starting_position
